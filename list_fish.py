1c5a4f278d03d0a324fa9f3e6c7f9c32b01cf0a9
kongminhao <2015141463077@stu.scu.edu.cn>
2019-12-01 08:42:44 +0800

    add search func

--- list_fish.py
+++ list_fish.py
@@ -4,19 +4,35 @@
 import workflow
 from workflow import web
 import sys
+import os
 
 wf = workflow.Workflow3()
 
 
 def get_fish_data():
+    from pypinyin import pinyin, Style
+
+    os.environ['PYPINYIN_NO_PHRASES'] = 'true'
     data = web.get('https://www.tophub.fun:8080/GetType').json()
+    for each in data["Data"]:
+        chinese_spell_list = pinyin(each['title'], strict=False, style=Style.FIRST_LETTER)
+        chinese_spell = ""
+        for e in chinese_spell_list:
+            chinese_spell += e[0]
+        each["chinese_spell"] = chinese_spell.lower()
     return data
 
 
 def main(wf):
     data = wf.cached_data('fish', get_fish_data, max_age=3600)
+    # data = get_fish_data()
     for each in data["Data"]:
-        wf.add_item(each['title'], arg=each['id'], autocomplete=each['id'], icon="fish.png", valid=False)
+        if len(sys.argv) == 1:
+            wf.add_item(each['title'], arg=each['id'], autocomplete=each['chinese_spell'] + " " + each['id'],
+                        icon="fish.png", valid=False)
+        elif each['chinese_spell'].startswith(sys.argv[1]):
+            wf.add_item(each['title'], arg=each['id'], autocomplete=each['chinese_spell'] + " " + each['id'],
+                        icon="fish.png", valid=False)
 
     wf.send_feedback()
 
